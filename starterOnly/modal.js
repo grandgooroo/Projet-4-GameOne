fa35b151ffb1e081b5442e06e01ec1f6744510c2
grandgooroo <grandgooroo@gmail.com>
2022-10-06 11:47:37 +0200

    Modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,65 +12,77 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const formCloseBtn = document.querySelectorAll(".close");
+const textControl = document.getElementsByClassName("text-control");
+const form = document.querySelector("form");
 
 // Get Form inputs
-let firstName = document.getElementById("firstName");
-    Name = document.getElementById("name");
-    eMail = document.getElementById("mail");
-    birthDate = document.getElementById("birthDate");
-    erreur = document.getElementsByClassName(".errorMsg");
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   checkInputs();
-// });
-
-// function checkInputs(); {
-//   // get the values from the inputs
-//   const firstNameValue = firstName.value.trim();
-//   const nameValue = Name.value.trim();
-//   const emailValue = eMail.value.trim();
-//   const birthValue = birthDate.value.trim();
-
-//   if(firstNameValue === '') {
-//     // show error message
-//     //add error class
-//     setErrorFor(firstNameValue, "Le champ ne peux être vide")
-  
-//   } else {
-//     //add succes class
-//   }
-// // Mail checkInputs
-//   if(emailValue === '') {
-//     setErrorFor(email, "Email cannot be blank");
-//   } else if(!isEmail(emailValue)) {
-//     setErrorFor(email, "Email is not valid");
-//   } else {
-//     setSuccessFor(email)
-//   }
-// }
+const firstName = document.getElementById("first");
+const Name = document.getElementById("last");
+const nameReg = new RegExp(/^[A-zÀ-ú-']{2,}$/);
+const eMail = document.getElementById("mail");
+const emailReg = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/);
+const birthDate = document.getElementById("birthDate");
+const erreur = document.getElementsByClassName(".errorMsg");
 
 
-function setErrorFor(input, message) {
-  const formData = input.parentElement; // .form-control
-  const small = formData.querySelector("small");
+form.addEventListener("submit", (e) => 
+{
+  if (emptyForm() === true) 
+  {
+    addValidationMsg();
+  }
+  e.preventDefault();
 
-  // add error message inside small
-  small.innerText = message;
+  invalidForm()
+});
 
-  // add error class
-  formData.className = "form-control error";
+function emptyForm()
+{
+  let checkEmpty = document.forms["first"]["name"]["mail"].value;
+  if (checkEmpty == "")
+  {
+    console.log("Stop le script");
+    alert ("Merci de renseigner les champs vides.");
+    return false;
+  }
+  return true;
 }
 
-function setSuccessFor(input) {
-  const formData = input.parentElement; // .form-control
-  formData.className = "form-copntrol sucess";
+function addValidationMsg() {
+  alert("Ok");
+}
 
-function isEmail(email) {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+function invalidForm() 
+{
+  const firstNameValue = firstName.value.trim(); // Vérifier les espacements
+  const NameValue = Name.value.trim();
+  const eMailValue = eMail.value.trim(); 
+};
+
+function validateFirstName()
+{
+  let vname = firstName.value;
+  if(vname.length == 0)
+  {
+    firstNameError.getElementById("first").innerHTML = "Le champs ne doit pas être vide";
+    return false;
+  }
+  if(!firstName.matches(/^[A-zÀ-ú-']{2,}$/))
+  {
+    firstNameError.getElementById("first").innerHTML = "Veuillez rentrer un prenom valide";
+    return false;
+  }
+  firstNameError.getElementById("first").innerHTML = "Valide"
+  return true;
 }
+
+function validate()
+{
+  alert("Inscription validé");
 }
-//  --- 
+
+//----------------------------------------------------------------
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
