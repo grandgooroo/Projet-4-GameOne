e5f0e0f776a2d81b05066efa3fa82a743fc2792f
grandgooroo <grandgooroo@gmail.com>
2022-11-08 11:34:02 +0100

    Before refactor

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,212 +22,199 @@ const first = document.getElementById("first");
 const Name = document.getElementById("name");
 const nameReg = new RegExp(/^[A-zÀ-ú-']{2,}$/);
 const eMail = document.getElementById("mail");
-const emailReg = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/);
+const emailReg = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/);
 const birthDate = document.getElementById("birthDate");
-const msgError = document.getElementsByClassName("msgerror");
+const birthDateReg = new RegExp(/^[0-9]{2}-[0-9]{2}-[0-9]{4}$/);
+const quantityReg = new RegExp(/^[0-9]{1,2}$/);
+const quantity = document.getElementById("quantity");
+const city = document.querySelectorAll("input[type=radio]");
+const cityN = document.querySelectorAll("input[name=location]"); //
+const generalCondition = document.getElementById("checkbox1");
+const newsletter = document.getElementById("checkbox2");
 
-const toto = document.getElementsByClassName("toto");
+const msgError = document.getElementsByClassName("msgerror");
 
 const isRequired = value => value === '' ? false : true;
 const isBetween = (length, min, max) => length < min || length > max ? false : true;
 
 // Form start
 
-
-
-
-
-// form.addEventListener("focus", (e) => {
-//   e.target.style.background = "red";
-// }, true);
-
-// form.addEventListener("blur", (e) => {
-//   e.target.style.background = "";
-// }, true);
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-    
-  // let checkIsEmpty = formData.forEach((input) => 
-  // {
-  //   if (input.value === "") {
-  //     alert("Veuillez remplir le formulaire");
-  //     console.log("Veuillez remplir le formulaire");
-  //     return false;
-  //   }
-  // })
-  while (input.value === "") {
-    alert("Veuillez remplir le formulaire");
-    return false;
-  }
   
+  // clear error messages
+  let clear = document.querySelectorAll(".msgerror"); // Clear
+  if (clear != null)
+  {
+  clear.forEach(msgerror => {
+    msgerror.remove();
+    console.log("masquer les messages");
+  });
+  }
+
+  function borderInputFalse() 
+  {
+    input.style.border = "medium solid #e54858";
+  }
+
+
     let firstNameInput = document.querySelector("#firstName input");
     let lastNameInput = document.querySelector("#name input");
     let emailInput = document.querySelector("#mail input");
-    let birthDate = document.querySelector("#birthDate input");
-    
+    let birthDateInput = document.querySelector("#birthDate input");
+    let cityMsg = document.getElementsByClassName("city"); // Selectionner le div avec une classe !?
+    let cguMsg = document.getElementsByClassName("checkbox2-label");
+
+
+    // Firts name input
     if (firstNameInput.value === "") // Test if first name is empty
     {
       let p = document.createElement("p");
       p.className = "msgerror";
-      p.textContent = "Le champ Prenom est obligatoire";
-      firstNameInput.after(p); 
+      p.textContent = "Le champ Prenom est obligatoire.";
+      firstNameInput.after(p);
+      // document.getElementsByClassName(".text-control", "true"); // Show red border
       console.log("test first name");
-      return false;
-    } 
-    // else if (!firstNameInput.value(nameReg))
-    // {
-    //   let p = document.createElement("p");
-    //   p.className = "msgerror";
-    //   p.textContent = "Veuillez renseigner un Prenom valide (2 characters minimum)";
-    //   firstNameInput.after(p); 
-    // }
+    } else {
+
+      if (!nameReg.test(firstNameInput.value))
+      {
+        let p = document.createElement("p");
+        
+        p.className = "msgerror";
+        p.textContent = "Le Prenom doit avoir deux characters minimum.";
+        firstNameInput.after(p);
+        
+        firstName.dataset.errorVisible = "true"; // Red border On
+        console.log("Prenom non valide"); // Test if first name match with Regex
+      } else {
+        firstName.dataset.errorVisible = "false"; // Red border Off // desactiver Border rouge !!!
+      }
+    }
+
+    // Last name input
     if (lastNameInput.value === "") // Test if last Name is empty
     {
       let p = document.createElement("p");
+
       p.className = "msgerror";
-      p.textContent = "Le champ Nom est obligatoire";
+      p.textContent = "Le champ Nom est obligatoire.";
       lastNameInput.after(p); 
       console.log("test last name");
-      return false;
-    }
+    } else {
 
+      if (!nameReg.test(lastNameInput.value))
+      {
+        let p = document.createElement("p");
+
+        p.className = "msgerror";
+        p.textContent = "Le Nom doit avoir deux characters minimum.";
+        lastNameInput.after(p);
+        console.log("Nom non valide"); // Test if last name match with Regex
+      }
+    }
+    // Email  input
     if (emailInput.value === "") // Test if E-mail is empty
     {
       let p = document.createElement("p");
+
       p.className = "msgerror";
-      p.textContent = "Le champ E-mail est obligatoire";
+      p.textContent = "Le champ E-mail est obligatoire.";
       emailInput.after(p); 
-      console.log("test e-mail");
-      return false;
+      console.log("test e-mail vide");
     }
-
-    if (birthDate.value === "") // Test if Birth Date is empty
+    
+    // if(!eMail.match(emailReg))
+    if (!emailReg.test(emailInput.value))
     {
-      let p = document.createElement("p");
-      p.className = "msgerror";
-      p.textContent = "Le Date de naissance est obligatoire";
-      birthDate.after(p); 
-      console.log("test date");
-      return false;
+    alert("Veuiller entrer une adresse e-mail valide.");
+    console.log("test e-mail valide");
     }
-    
-    
-  // if (location1.checked) {// Fonction de validation de coches
-  //   // msgError.innerHTML = "ta maman";
-  // } else {
-  //   alert("Veuillez cocher une ville")
-  // }
-    // validate the form
+
+    // Check birthDate
+  
+  let date = new Date(document.getElementById("birthdate").value);
+  let today = new Date();
+
+  if (!birthDateInput.value) 
+  {
+    alert("Veuiller renseigner une date");
+
+    let p = document.createElement("p");
+
+    p.className = "msgerror";
+    p.textContent = "Veuiller renseigner une date valide.";
+    birthDateInput.after(p); 
+  }
+
+  if (date > today)
+  {
+    console.log("Date incorrecte"); 
+  }
+  // if (!birthDateReg.test(birthDateInput.value)) // Ajouter contrainte 4 chiffres Regex.
+  // {
+  //   console.log("Année incorrecte");
+  // } 
+  else {
+    alert ("Date correcte"); 
+  }
+  
+
+    // Check Tournament
+let quantityMax = document.getElementById("quantity").max = 9;
+
+ if (quantity.value.length !== "") // Test if Tournament is empty
+    {
+      console.log("Tournament OK");
+    } 
+
+    // Check  City
+  // let isCityChecked = city.checked === false;
+
+  // console.log(isCityChecked);
+  // if (!isCityChecked)
+
+let count = 0;
+  for (let i = 0; i < city.length; i++)
+  {
+    if (city[i].checked) 
+    {
+      count++;
+    console.log("Ville OK");
+    }
+  }
+  if (!count)
+  {
+  console.log("Veuiller cocher une ville");
+  }
+
+      // Check CGU
+  if (generalCondition.checked)
+  {}  else {
+    console.log("Veuillez cocher les CGU");
+    // let p = document.createElement("p");
+    // p.className = "msgerror";
+    // p.textContent = "Veuiller renseigner une date valide.";
+    // cguMsg.after(p);
+  }
+
+    // Check Newletter
+  if (newsletter.checked)
+  {}  else {
+    console.log("Newsletter Non coché");
+  }
+
+// Send form // Si tous les champs sont valides alors fermer la modal
+// Show validation message
+// merci()
+// console.log("Affiche message Merci");
 });
-
-// // Check if form is not empty
-// function checkInputs()
-// {
-//   const firstNamevalue = first.value.trim();
-//   const Namevalue = Name.value.trim();
-
-//   if(firstValue === "") {
-//     // show error message
-    
-
-//     // add error className
-//     setErrorFor(first, "Veuillez rensigner un prénom")
-//   } else
-//     {
-// // add success className
-//       setSuccessFor(first);
-//     }
-// }
-
-// function setErrorFor(input, message)
-// {
-//   const formControl = input.parentElement; // .form-control
-//   const showErrorMsg = formControl.querySelector("formData");
-//   //add error message inside input
-//   formData.innerText = message;
-//   //add error className
-//   formControl.className = "data-error-visible";
-// }
-
-// Set validation input message
-// function setValidationMessage(element, message)
-// {
-//   element.setAttribute("data-error-visible", true);
-//   element.setAttribute("data-error", message);
-// }
-// // Clear validation input message
-// function clearValidationMessage(element) 
-// {
-//   element.setAttribute("data-error-visible", false);
-//   element.setAttribute("data-error", "");
-// }
-
-
-// form.addEventListener("input", function (e) {
-//   switch (e.target.id) {
-//       case "first":
-//           checkfirstName();
-//           break;
-//       case "last":
-//           checkLast();
-//           break;
-//       case "mail":
-//           checkEmail();
-//           break;
-//   }
-// });
-
-// firstName.addEventListener("input", checkfirstName);
-
-// function checkfirstName () {
-
-//   if(firstName.length == 0)
-//     {
-//     alert("Veuillez renseigner votre prenom");
-//     firstName.innerHTML = "ta maman";
-//     return true;
-//     }
-// };
-// function validateFirstName = (firstName) =>
-// {
-//   let vName = firstName.value;
-//   let vName = document.getElementById("first").innerHTML;
-//   if(vName.length == 0)
-//     {
-//     alert("Veuillez renseigner votre prenom");
-//     // document.firstName.style.background = "Yellow";
-//     // firstName.setAttribute("data-error-visible", "true");
-//     // firstName.setAttribute("data-error", "toto");
-//     return false;
-//     }
-//   // {
-//   //   firstNameError.getElementById("first").innerHTML = "Le champs ne doit pas être vide";
-//   //   firstNameError.getElementById("first").value = "Le champs ne doit pas être vide";
-//   //   document.getElementById("first").textContent = "Le champs ne doit pas être vide";
-//   //   setValidationMessage(firstName, "toto")
-//   // }
-//   if(!firstName.matches(/^[A-zÀ-ú-']{2,}$/))
-//   {
-//     firstNameError.getElementById("first").innerHTML = "Veuillez rentrer un prenom valide";
-//   }
-//   return true;
-// }
-
-// function validateEmail()
-// {
-//   // if(!eMail.matches(emailReg))
-//   if (emailReg.test(eMail) === false)
-//   alert("Veuiller entrer une adresse e-mail valide");
-//   return true;
-//   // {
-//   //   emailError.getElementById("email").innerHTML = "Veuillez rentrer une adresse e-mail valide";
-//   // }
-//   // returne true;
-// }
-
 //----------------------------------------------------------------
-
+function merci()
+{
+let modal = document.querySelector(".modal-body");
+modal.innerHTML = "Merci";
+}
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
